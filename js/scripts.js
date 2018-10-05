e0c168d4f864cb94ab03a3de42e2c208305e8433
Nicole Cox <nicolecox@Nicoles-MacBook-Pro.local>
2018-10-04 19:05:33 -0700

    Working on jquery to return a number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
 //buisness logic
-$(document).ready(function() {
+$(document).ready(function(){
   $("#blanks").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($("#user").val());
-    var numbers = 0;
+    var numbers = [];
+    for (var i = 1; i <= userInput; i+++){
+      numbers.push(i);
+     });
+    });
   });
-});
-
 
 
 //user interface logic
-var outputs = numbers.map(function(number){
-  if (number % 3 === 0 && number % 5 === 0)
-  return number = "ping-pong";
-} else if (number % 3 === 0) {
-  return number = "ping";
-}else if (number % 5 === 0){
-return number ="pong";
-} else {
-  return number;
-});
+var outputs = numbers.forEach(function(number){
+    if (number % 3 === 0 && number % 5 === 0){
+    return number = "ping-pong";
+  } else if (number % 3 === 0){
+    return number = "ping";
+  }else if (number % 5 === 0){
+  return number ="pong";
+  } else  {
+    return number = "please enter a number";
+    }
+  });
+
+
+outputs.forEach(function(output){
+     $("ul").append("<li>" + output + "</li>");
+   });
