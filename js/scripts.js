8451f7e1598ee2165470bf030d924bc6275f97e2
Nicole Cox <nicolecox@Nicoles-MacBook-Pro.local>
2018-10-04 17:57:18 -0700

    This will put the if else if's into the user interface logic of scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,13 @@ $(document).ready(function() {
 
 
 //user interface logic
+var outputs = numbers.map(function(function){
+  if (number % 3 === 0 && number % 5 === 0)
+  return number = "ping-pong"
+} else if (number % 3 === 0) {
+  return number = "ping";
+}else if (number % 5 === 0){
+return number ="pong";
+} else {
+  return number;
+});
