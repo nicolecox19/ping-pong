84c0b02b17f05d9d9f7da68420f4196e049f723d
Nicole Cox <nicolecox@Nicoles-MacBook-Pro.local>
2018-10-06 20:10:04 -0700

    This will allow the website to spit a answer out after you hit submit and also moves the answer below the submit button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,27 @@
-//buisness logic
 $(document).ready(function(){
   $("#blanks").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($("#user").val());
     var numbers = [];
-    for (var i = 1; i <= userInput; i+++){
+    for (var i = 1; i <= userInput; i++) {
       numbers.push(i);
-     });
-    });
-  });
-
-
-//user interface logic
-var outputs = numbers.forEach(function(number){
-    if (number % 3 === 0 && number % 5 === 0){
-    return number = "ping-pong";
-  } else if (number % 3 === 0){
-    return number = "ping";
-  }else if (number % 5 === 0){
-  return number ="pong";
-  } else  {
-    return number = "please enter a number";
     }
+
+    var outputs = numbers.map(function(number){
+    	if (number % 3 === 0 && number % 5 === 0) {
+      	return number = "ping-pong"
+      } else if (number % 3 === 0) {
+      	return number = "Ping";
+      } else if (number % 5 === 0) {
+      	return number = "Pong";
+      } else {
+      	return number;
+      }
+    });
+
+    outputs.forEach(function(output){
+      $("ul").append("<li>" + output + "</li>");
+    });
+
   });
-
-
-outputs.forEach(function(output){
-     $("ul").append("<li>" + output + "</li>");
-   });
+});
